fa88b1593778836239aec835abe9d39e5760cb65
MarcoCPinho <marcocpinho@hotmail.com>
2020-08-13 15:54:21 -0300

    Update index.tsx

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -6,12 +6,12 @@ import backIcon from '../../assets/images/icons/back.svg';
 
 import './styles.css';
 
-interface PageHeaderProps {
+interface PageHeaderProps { //esta interface irá forçar a alteração da funcao PageHeader para uma FC
     title: string; /* automaticamente entende que é uma propriedade obrigatória */
     description?: string; /*o "?:"informa que não uma propriedade obrigatória*/
 }
 
-const PageHeader: React.FunctionComponent<PageHeaderProps> = (props) => {
+const PageHeader: React.FunctionComponent<PageHeaderProps> = (props) => { //PageHeader (":" -> é do tipo) é do tipo React.FunctionComponent informando as propriedade tipadas que o input pode receber (propriedades do "<PageHeaderProps>" no caso acima).... .... (props) -> recebo todas as propriedades... se eu quiser receber alguma em especifico, posso sempre fazer uma desestruturação ({ title }) -> pego so o title e ignoro o description, neste exemplo
     return (
         <header className="page-header">
             <div className="top-bar-container">
